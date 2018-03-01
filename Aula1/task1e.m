e3fb6e31e599caed6ff0bea53a2926d5c59972d7
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2018-03-01 11:54:38 +0000

    1e done

--- Aula1/task1e.m
+++ Aula1/task1e.m
@@ -3,22 +3,23 @@ timegap = 10*10^(-6);
 fcs = 4;
 header = 36;
 
-q=logspace(-8,-3,7900);
+q=logspace(-8,-3,100);
+
+aux = zeros(7900,100);
+
 p=0.025;
 
-A = (((0:7900)+100)*8)/link;
-B = timegap+A+((header*8)/link)+((fcs*8)/link);
-datarate = 54*(A/B);
+for n=0:7900
+    A = (((n)+100)*8)/link;
+    B = timegap+A+((header*8)/link)+((fcs*8)/link);
+    datarate = 54*(A/B);
+    x = ((1-q).^(n+100*8)).*datarate;
+    aux(n+1,:) = x*p*((1-p)^n);
+end
+res = sum(aux);
 
-f = ((1-q).^((((0:7900)+100)+40)*8))*datarate;
-
-% probabilidades para bytes de 100 a 8000 
-% parece que as probabildades dão 0, mas são é muito pequenas, no total dá 1
-pTotal = f.*[p*(1-p).^(0:1:7900)];
-
-pTotal = sum(pTotal);
-
-semilogx(q,pTotal)
+semilogx(q,res,'k')
 grid on
 xlabel('Data Field (Bytes)')
 ylabel('Data Rate (Seconds)')
+
