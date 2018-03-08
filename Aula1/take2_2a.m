cafc4c43d4339a2f31b2a8611ca1a71381718df3
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2018-03-08 12:46:36 +0000

    aula 1 updated aula 2 InitialRandom and UpdateCoordinates done

--- Aula1/take2_2a.m
+++ Aula1/take2_2a.m
@@ -2,10 +2,7 @@
 % N - estado normal
 % E - recebida com erros
 
-p1 = 0.99;
-p2 = 0.999;
-p3 = 0.9999;
-p4 = 0.99999;
+p = [0.99 0.999 0.9999 0.99999];
 
 errors_normal_state = 0.01/100; % P(E/N)
 errors_interference_state = 50/100; % P(E/I)
@@ -19,11 +16,12 @@ errors_interference_state = 50/100; % P(E/I)
 % P(I/E) = P(E/I)*P(I)/P(E)
 % P(E) = P(E/I)*P(I) + P(E/N)*P(N)
 
-p_i1 = errors_interference_state*(1-p1)/(errors_interference_state*(1-p1) + errors_normal_state*p1)
-p_i2 = errors_interference_state*(1-p2)/(errors_interference_state*(1-p2) + errors_normal_state*p2)
-p_i3 = errors_interference_state*(1-p3)/(errors_interference_state*(1-p3) + errors_normal_state*p3)
-p_i4 = errors_interference_state*(1-p4)/(errors_interference_state*(1-p4) + errors_normal_state*p4)
+for i=1:length(p)
 
+p_i(i) = errors_interference_state*(1-p(i))/(errors_interference_state*(1-p(i)) + errors_normal_state*p(i));
+p_ni(i) = 1 - p_i(i);
+
+end
 % conclusao: quanto maior a probabilidade de estar em estado normal, menor
 % a possibilidade de estar em estado de interferência dado que recebeu uma
 % trama com erros
@@ -31,7 +29,5 @@ p_i4 = errors_interference_state*(1-p4)/(errors_interference_state*(1-p4) + erro
 % agora vamos calcular a probabilidade de estar no estado normal dado que
 % recebeu uma frame com erros
 
-p_n1 = 1 - p_i1
-p_n2 = 1 - p_i2
-p_n3 = 1 - p_i3
-p_n4 = 1 - p_i4
+p_i
+p_ni
--- Aula1/take2_2c.m
+++ Aula1/take2_2c.m
@@ -1,82 +1,27 @@
 % P(N) = p(1,2,3,4)
 % P(I) = 1 - p(1,2,3,4)
 
-
 errors_normal_state = 0.01/100; % P(E/N)
 errors_interference_state = 50/100; % P(E/I)
 
-pN_n1 = 1 - errors_normal_state^2;  % P(E/N)
-pN_n2 = 1 - errors_normal_state^3;  % P(E/N)
-pN_n3 = 1 - errors_normal_state^4;  % P(E/N)
-pN_n4 = 1 - errors_normal_state^5;  % P(E/N)
+n = [2 3 4 5];
 
-pI_n1 = 1 - errors_interference_state^2;  % P(E/I)
-pI_n2 = 1 - errors_interference_state^3;  % P(E/I)
-pI_n3 = 1 - errors_interference_state^4;  % P(E/I)
-pI_n4 = 1 - errors_interference_state^5;  % P(E/I)
+for i=1:length(n)
+    pN_n(i) = 1 - errors_normal_state^(n(i));
+    pI_n(i) = 1 - errors_interference_state^(n(i));
+    
+end
 
+p = [0.99 0.999 0.9999 0.99999];
 
-% P(E) => probabilidade de serem recebidas frames com erros
-% P(I/E) = P(E/I)*P(I)/(P(E/I)*P(I) + P(E/N)*P(N))
-% P(E) = P(E/I)*P(I) + P(E/N)*P(N)
+for i=1:length(p)
+    piE2_N(i) = (pI_n(1)*(1-p(i)))/(pN_n(1)*p(i) + pI_n(1)*(1-p(i)));
+    piE3_N(i) = (pI_n(2)*(1-p(i)))/(pN_n(2)*p(i) + pI_n(2)*(1-p(i)));
+    piE4_N(i) = (pI_n(3)*(1-p(i)))/(pN_n(3)*p(i) + pI_n(3)*(1-p(i)));
+    piE5_N(i) = (pI_n(4)*(1-p(i)))/(pN_n(4)*p(i) + pI_n(4)*(1-p(i)));  
+end
 
-p1 = 0.99;
-p2 = 0.999;
-p3 = 0.9999;
-p4 = 0.99999;
-
-
-% P1
-% para n = 2
-p1E2_N = (pI_n1*(1-p1))/(pN_n1*p1 + pI_n1*(1-p1))
-
-% para n = 3
-p1E3_N = (pI_n2*(1-p1))/((pN_n2*p1) + pI_n2*(1-p1))
-
-% para n = 4
-p1E4_N = (pI_n3*(1-p1))/((pN_n3*p1) + pI_n3*(1-p1))
-
-%para n = 5
-p1E5_N = (pI_n4*(1-p1))/((pN_n4*p1) + pI_n4*(1-p1))
-
-% P2
-% para n = 2
-p2E2_N = (pI_n1*(1-p2))/((pN_n1*p2) + pI_n1*(1-p2));
-
-% para n = 3
-p2E3_N = (pI_n2*(1-p2))/((pN_n2*p2) + pI_n2*(1-p2));
-
-% para n = 4
-p2E4_N = (pI_n3*(1-p2))/((pN_n3*p2) + pI_n3*(1-p2));
-
-%para n = 5
-p2E5_N = (pI_n4*(1-p2))/((pN_n4*p2) + pI_n4*(1-p2));
-
-% P3
-% para n = 2
-p3E2_N = (pI_n1*(1-p3))/((pN_n1*p3) + pI_n1*(1-p3));
-
-% para n = 3
-p3E3_N = (pI_n2*(1-p3))/((pN_n2*p3) + pI_n2*(1-p3));
-
-% para n = 4
-p3E4_N = (pI_n3*(1-p3))/((pN_n3*p3) + pI_n3*(1-p3));
-
-%para n = 5
-p3E5_N = (pI_n4*(1-p3))/((pN_n4*p3) + pI_n4*(1-p3));
-
-% P4
-% para n = 2
-p4E2_N = (pI_n1*(1-p4))/((pN_n1*p4) + pI_n1*(1-p4));
-
-% para n = 3
-p4E3_N = (pI_n2*(1-p4))/((pN_n2*p4) + pI_n2*(1-p4));
-
-% para n = 4
-p4E4_N = (pI_n3*(1-p4))/((pN_n3*p4) + pI_n3*(1-p4));
-
-%para n = 5
-p4E5_N = (pI_n4*(1-p4))/((pN_n4*p4) + pI_n4*(1-p4));
-
-
-
+piE2_N
+piE3_N
+piE4_N
+piE5_N
