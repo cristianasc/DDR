befe6d99187b3a9727e544828851c8b313890ecf
Daniela Simões <danielasimoes@ua.pt>
2018-04-16 12:37:21 +0100

    Parte1 done.

--- Aula3/simulator.m
+++ Aula3/simulator.m
@@ -1,4 +1,7 @@
 
+%os valores teoricos (os da tabela da alinea a) devem estar dentro do valor
+%resultado do simulados ± o intervalo de confiança
+
 N = 10; %número de simulações
 results= zeros(1,N); %vetor com os N resultados de simulação
 results2 = zeros(1,N);
@@ -7,27 +10,41 @@ C =  [100 100 100 100 100 100 100 100 1000 1000 1000 1000 1000 1000 1000 1000];
 M = [4 4 4 4 10 10 10 10 4 4 4 4 10 10 10 10];
 R=5000;
 
-for it= 1:N
-    for i = 1:16
-        [results(it), results2(it)] = simulator1(lambda(i),C(i),M(i),R)
+
+for i = 1:16
+    for it= 1:N
+        [results(it), results2(it)] = simulator1(lambda(i),C(i),M(i),R);
     end
+    confidence_interval90(results, N, "b", i);
+    confidence_interval90(results2, N, "o", i);
+end
+
+function [termo] = confidence_interval90(results, N, type, i)
+    alfa= 0.1; %intervalo de confiança a 90%
+    media = mean(results);
+    termo = norminv(1-alfa/2)*sqrt(var(results)/N);
+    fprintf('resultado %s%d = %.2e +- %.2e\n', type, i, vpa(media),vpa(termo))
 end
 
 function [b o]= simulator1(lambda,C,M,R)
     %lambda = request arrival rate (in requests per hour) %C= Internet connection capacity (in Mbps)
     %M= throughput of each movie (in Mbps)
     %R= stop simulation on ARRIVAL no. R
+    
     invlambda=60/lambda; %average time between requests (in minutes) 
     invmiu= load('movies.txt'); %duration (in minutes) of each movie 
     Nmovies= length(invmiu); % number of movies
+    
     %Events definition:
     ARRIVAL= 0; %movie request
     DEPARTURE= 1; %termination of a movie transmission %State variables initialization:
     STATE= 0;
+    
     %Statistical counters initialization:
     LOAD= 0;
     NARRIVALS= 0;
     BLOCKED= 0;
+    
     %Simulation Clock and initial List of Events:
     Clock= 0;
     EventList= [ARRIVAL exprnd(invlambda)];
