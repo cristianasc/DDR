41aad3c664b39ab11d88520d784367fc68c40f60
Daniela Simões <danielasimoes@ua.pt>
2018-05-28 13:00:13 +0100

    fucntions ok but 1g not completed.

--- Aula4/Evaluate.m
+++ Aula4/Evaluate.m
@@ -10,12 +10,15 @@ function [AverageDelay,max_delay]  = Evaluate(CurrentSolution, lambda)
     AverageDelay = (lambda./(miu-lambda)+lambda.*d);
     AverageDelay(isnan(AverageDelay)) = 0;
     AverageDelay = sum(sum(AverageDelay))/gama;
-    AverageDelay = AverageDelay*2 %ida e volta => descomentar para ver o resultado
+    AverageDelay = AverageDelay*2; %ida e volta => descomentar para ver o resultado
 
     delay_flows = zeros(nT,1);
 
     for i=1:nT
+        origin= T(i,1);
         destination= T(i,2);
+        r= ShortestPathSym(d,origin,destination); 
+        CurrentSolution(i,:)= r;
         j=1;
         while CurrentSolution(i, j)~= destination
             delay_flows(i) = delay_flows(i) + (1/(miu(CurrentSolution(i, j),CurrentSolution(i, j+1))-lambda(CurrentSolution(i, j),CurrentSolution(i, j+1))) + d(CurrentSolution(i, j),CurrentSolution(i, j+1)));
@@ -24,5 +27,5 @@ function [AverageDelay,max_delay]  = Evaluate(CurrentSolution, lambda)
         end
     end
     
-    max_delay = max(delay_flows)
+    max_delay = max(delay_flows);
 end
