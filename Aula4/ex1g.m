2e54e0010d83e133f533d96f1ece389a3818f091
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2018-05-31 10:48:12 +0100

    ex1g. falta gráfico

--- Aula4/ex1g.m
+++ Aula4/ex1g.m
@@ -1,42 +1,38 @@
 % average delay for menor ou igual, e neste caso se for igual entao o max
 % delay tem de ser também menor
-Counter = 0
+Counter = 0;
 GlobalBest = Inf;
 while Counter < 20
     [CurrentSolution, lambda] = GreedyRandomized(); 
-    [AverageDelay, max_delay] = Evaluate(CurrentSolution, lambda); 
+    [CurrentObjective1, CurrentObjective2] = Evaluate(CurrentSolution, lambda);  %AverageDelay
     repeat= true;
     while repeat
          NeighbourBest= Inf;
          for i=1:size(CurrentSolution,1)
              NeighbourSolution= BuildNeighbour(CurrentSolution,lambda, i); 
-             [AverageDelayNeighbor, max_delayNeighbor] = Evaluate(NeighbourSolution, lambda);
-             if AverageDelayNeighbor <= NeighbourBest
-                 if AverageDelayNeighbor == NeighbourBest
-                     if max_delayNeighbor < max_delay
-                        max_delay = max_delayNeighbor;
-                        AverageDelay = AverageDelayNeighbor;
-                        %NeighbourBest= NeighbourObjective;
-                        NeighbourBestSolution= NeighbourSolution;
-                     end
-                 else
-                     max_delay = max_delayNeighbor;
-                     AverageDelay = AverageDelayNeighbor;
-                     %NeighbourBest= NeighbourObjective;
-                     NeighbourBestSolution= NeighbourSolution;
+             [NeighbourSolution1, NeighbourSolution2] = Evaluate(NeighbourSolution, lambda);
+             if NeighbourSolution1 < NeighbourBest 
+                 CurrentObjective2 = NeighbourSolution2;
+                 CurrentObjective1 = NeighbourSolution1;
+                 NeighbourBestSolution = NeighbourSolution;  
+             elseif NeighbourSolution1 == NeighbourBest
+                 if NeighbourSolution2 < CurrentObjective2
+                    CurrentObjective2 = max_delayNeighbor;
+                    CurrentObjective1 = NeighbourSolution1;
+                    NeighbourBestSolution= NeighbourSolution;
                  end
              end
          end
-         if NeighbourBest < CurrentObjective 
-             CurrentObjective = NeighbourBest; 
-             CurrentSolution= NeighbourBestSolution;
+         if NeighbourBest < CurrentObjective1 || (NeighbourBest == CurrentObjective1 && CurrentObjective2 < NeighbourSolution2)
+             CurrentObjective1 = NeighbourBest;
+             CurrentSolution = NeighbourBestSolution;
          else
              repeat= false;
          end
      end
-     if CurrentObjective < GlobalBest
-         GlobalBestSolution= CurrentSolution;
-         GlobalBest= CurrentObjective;
+     if CurrentObjective1 < GlobalBest || (CurrentObjective1 == GlobalBest &&  NeighbourSolution2 < CurrentObjective2) 
+         GlobalBestSolution = CurrentSolution;
+         GlobalBest = CurrentObjective1
          Counter = 0;
      else
          Counter = Counter + 1;
