73af8b0ecb5d13f66eb63d9521e7b26dcbc18f98
Cristiana Carvalho <cristiana96carvalho@hotmail.com>
2018-06-08 18:03:10 +0100

    BuildNeighbour bugfix

--- Aula4/BuildNeighbour.m
+++ Aula4/BuildNeighbour.m
@@ -1,31 +1,31 @@
-function r = BuildNeighbour(CurrentSolution, lambda, i)
+function [CurrentSolution,lambda] = BuildNeighbour(CurrentSolution, lambda, i)
     Matrizes;
     T(:,3:4)= T(:,3:4)*1e6/(8*1000);
     d= L*1e3/2e8;
     miu= R*1e9/(8*1000);
     nT= size(T,1);
     delay_flows = zeros(nT,1);
-    rtd = 1./(miu-lambda) + d;
     origin= T(i,1);
  
     r = CurrentSolution(i,:);
     destination= T(i,2);
-    
+    lambda_od= T(i,3);
+    lambda_do= T(i,4);
     j=1;
-    while CurrentSolution(i, j)~= destination
-        delay_flows(i) = delay_flows(i) - (1/(miu(CurrentSolution(i, j),CurrentSolution(i, j+1))-lambda(CurrentSolution(i, j),CurrentSolution(i, j+1))) + d(CurrentSolution(i, j),CurrentSolution(i, j+1)));
-        delay_flows(i) = delay_flows(i) - (1/(miu(CurrentSolution(i, j+1),CurrentSolution(i, j))-lambda(CurrentSolution(i, j+1),CurrentSolution(i, j))) + d(CurrentSolution(i, j+1), CurrentSolution(i, j)));
+    while r(j)~= destination
+        lambda(r(j),r(j+1))= lambda(r(j),r(j+1)) - lambda_od; 
+        lambda(r(j+1),r(j))= lambda(r(j+1),r(j)) - lambda_do; 
         j= j+1;
     end
     
+    rtd = 1./(miu-lambda) + d;
     r = ShortestPathSym(rtd,origin,destination); 
     CurrentSolution(i,:)= r;
     
     j=1;
-    while CurrentSolution(i, j)~= destination
-        delay_flows(i) = delay_flows(i) + (1/(miu(CurrentSolution(i, j),CurrentSolution(i, j+1))-lambda(CurrentSolution(i, j),CurrentSolution(i, j+1))) + d(CurrentSolution(i, j),CurrentSolution(i, j+1)));
-        delay_flows(i) = delay_flows(i) + (1/(miu(CurrentSolution(i, j+1),CurrentSolution(i, j))-lambda(CurrentSolution(i, j+1),CurrentSolution(i, j))) + d(CurrentSolution(i, j+1), CurrentSolution(i, j)));
+    while r(j)~= destination
+        lambda(r(j),r(j+1))= lambda(r(j),r(j+1)) + lambda_od; 
+        lambda(r(j+1),r(j))= lambda(r(j+1),r(j)) + lambda_do; 
         j= j+1;
     end
-    
 end
